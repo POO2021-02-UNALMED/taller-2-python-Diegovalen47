7881c93f54bcd80b7e2d65ddd269e6e449795480
Valentín Osorio <dosoriom@unal.edu.co>
2021-10-24 00:27:46 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,67 @@
+class Asiento:
+
+    def __init__(self,color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        if color == "rojo" :
+            self.color = color
+        elif color == "verde" :
+            self.color = color
+        elif color == "amarillo" :
+            self.color = color
+        elif color == "negro" :
+            self.color = color
+        elif color == "blanco" :
+            self.color = color
+
+class Motor:
+
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        if tipo == "electrico":
+            self.tipo = tipo
+        elif tipo == "gasolina":
+            self.tipo = tipo
+
+class Auto:
+
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
+    def cantidadAsientos():
+
+        cantidad = 0
+        for i in self.asientos:
+            if self.asientos[i] != None:
+                cantidad += 1
+
+        return cantidad
+
+    def verificarIntegridad():
+
+        if self.motor.registro != self.registro and self.motor != None:
+            return "Las piezas no son originales"
+
+        for i in self.asientos:
+
+            if self.asientos[i] != None and self.asientos[i].registro != self.registro:
+                return "Las piezas no son originales"
+        
+        return "Auto original"
